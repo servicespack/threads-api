3a8eec77d6397618587f6515cb4846507ce2fb05
Gabriel Rufino <contato@gabrielrufino.com>
2023-11-11 15:41:27 -0300

    Initial 'get thread' operation

--- controllers/get_thread.go
+++ controllers/get_thread.go
@@ -1,12 +1,22 @@
 package controllers
 
 import (
+	"github.com/servicespack/threads-api/configs"
+	"github.com/servicespack/threads-api/models"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func GetThread(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"get": true,
-	})
+	id := context.Param("id")
+
+	db := configs.GetDB()
+	var thread models.Thread
+	err := db.First(&thread, id).Error
+	if err != nil {
+		context.JSON(http.StatusNotFound, err)
+		return
+	}
+
+	context.JSON(http.StatusOK, thread)
 }
