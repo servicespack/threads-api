9d0ba7ead666aec4f7a2bce77611b1c8e299e8ac
Gabriel Rufino <contato@gabrielrufino.com>
2024-03-30 17:52:51 -0300

    wip: delete thread

--- controllers/delete_thread.go
+++ controllers/delete_thread.go
@@ -1,12 +1,24 @@
 package controllers
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/servicespack/threads-api/configs"
+	"github.com/servicespack/threads-api/models"
 )
 
 func DeleteThread(context *gin.Context) {
+	id := context.Param("id")
+	db := configs.GetDB()
+
+	err := db.Delete(&models.Thread{}, id)
+	if err != nil {
+		context.JSON(http.StatusNotFound, err)
+		return
+	}
+
 	context.JSON(http.StatusOK, gin.H{
-		"updated": true,
+		"deleted": true,
 	})
 }
